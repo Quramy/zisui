8d899bcd7b70b59ea3ae4f39d1d95b5b16e6d1af
Yosuke Kurami <yosuke.kurami@gmail.com>
2018-08-16 01:44:47 +0900

    rename exaple dir

--- e2e.sh
+++ e2e.sh
@@ -5,7 +5,6 @@ function run() {
   yarn --pure-lockfile
   yarn clear
   yarn zisui:all
-  code=$?
   img_count=$(find __screenshots__ -name "*.png" | wc -l)
   if [ "$img_count" -eq 0 ]; then
     echo "Test was failed. There is no capture..."
@@ -13,10 +12,14 @@ function run() {
     exit 1
   fi
   popd > /dev/null
-  exit $code
+  echo "Success $1"
 }
 
-run examples/simple-example || exit 1
-run examples/react-example || exit 1
+run examples/simple-example && \
+  run examples/managed-react
+
+if [ "$?" -gt 0 ]; then
+  exit 1
+fi
 
 echo "E2E test was ended successfully 🎉"
