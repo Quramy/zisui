d1b154b8b02bc8fc04a827ea850b9f7f86ae8452
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-03-31 23:02:27 +0900

    modify example story

--- examples/v5-managed-react/src/stories/index.js
+++ examples/v5-managed-react/src/stories/index.js
@@ -5,8 +5,14 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import { withScreenshot } from 'zisui';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome_override', module)
+.addDecorator(withScreenshot({
+  viewport: 'iPhone 6',
+ }))
+.add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
@@ -17,3 +23,7 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+storiesOf('Button_to_be_skipped', module)
+.addDecorator(withScreenshot({ skip: true }))
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
